f20cafd17051e6fbbe1bf452d7a1d1e06f1cbbcd
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-09-10 07:16:13 -0300

    feat(sign in): use react hook form in form inputs

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,43 @@
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CircleAlert } from "lucide-react";
+import * as z from "zod";
+
 import { Input } from "../components/form/Input";
 import { Button } from "../components/Button";
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInSchema = z.object({
+  email: z.email("E-mail inválido").toLowerCase(),
+  password: z.string("Senha obrigatória").nonempty("Senha obrigatória"),
+});
+
 export function SignIn() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(signInSchema),
+  });
+
+  function onSubmit(data: SignInFormData) {
+    console.log(data);
+  }
+
   return (
-    <form className="w-full flex flex-col gap-3">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full flex flex-col gap-3"
+    >
       <div className="border border-gray-500 p-6 rounded-[.625rem]">
         <h1 className="text-xl text-gray-200 font-bold mb-[.125rem]">
           Acesse o portal
@@ -14,31 +48,50 @@ export function SignIn() {
 
         <div className="my-8 flex flex-col gap-4">
           <div>
-            <Input
-              required
-              label="e-mail"
-              type="email"
-              placeholder="exemplo@email.com"
+            <Controller
+              control={control}
+              name="email"
+              render={({ field }) => (
+                <Input
+                  label="e-mail"
+                  type="email"
+                  placeholder="exemplo@email.com"
+                  {...field}
+                />
+              )}
             />
 
-            <span></span>
+            {errors.email?.message && (
+              <span className="text-feedback-danger flex items-center gap-1 mt-1">
+                <CircleAlert size={16} color="#d03e3e" />
+                {errors.email.message}
+              </span>
+            )}
           </div>
 
           <div>
-            <Input
-              required
-              label="senha"
-              type="password"
-              placeholder="Digite sua senha"
+            <Controller
+              control={control}
+              name="password"
+              render={({ field }) => (
+                <Input
+                  label="senha"
+                  type="password"
+                  placeholder="Digite sua senha"
+                  {...field}
+                />
+              )}
             />
-
-            <span></span>
+            {errors.password?.message && (
+              <span className="text-feedback-danger flex items-center gap-1 mt-1">
+                <CircleAlert size={16} color="#d03e3e" />
+                {errors.password.message}
+              </span>
+            )}
           </div>
         </div>
 
-        <Button>
-          Entrar
-        </Button>
+        <Button type="submit">Entrar</Button>
       </div>
 
       <div className="border border-gray-500 p-6 rounded-[.625rem]">
@@ -50,9 +103,7 @@ export function SignIn() {
           Cadastre agora mesmo
         </p>
 
-        <Button>
-          Criar conta
-        </Button>
+        <Button>Criar conta</Button>
       </div>
     </form>
   );
