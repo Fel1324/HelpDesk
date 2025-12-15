f85f61fb301d5f1994fc7f701b758b4977c42e37
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-12-15 20:33:45 -0300

    feat(customer pages): create textarea and select input components

---
+++ src/components/form/SelectInput.tsx
@@ -0,0 +1,25 @@
+type Props = React.ComponentProps<"select"> & {
+  id: string;
+  label: string;
+}
+
+export function SelectInput({ id, label, children, ...rest}: Props) {
+  return (
+    <div className="w-full flex flex-col text-gray-300 focus-within:text-blue-base">
+      <label className="text-xs uppercase font-bold text-inherit tracking-wider" htmlFor={id}>
+        {label}
+      </label>
+
+      <select
+        className="w-full h-[2.5rem] border-b border-gray-500 text-base text-gray-400 placeholder:text-base placeholder:text-gray-400 focus:outline-none focus:border-blue-base"
+        id={id}
+        {...rest}
+      >
+        <option className="text-base font-bold text-gray-400" value="" disabled hidden selected>
+          Selecione o serviço do chamado
+        </option>
+        {children}
+      </select>
+    </div>
+  )
+}
--- src/pages/customer/CreateTicket.tsx
+++ src/pages/customer/CreateTicket.tsx
@@ -1,9 +1,52 @@
+import { Input } from "../../components/form/Input";
+import { SelectInput } from "../../components/form/SelectInput";
+import { TextArea } from "../../components/form/TextArea";
+import { FormLayout } from "../../components/layouts/FormLayout";
 import { Title } from "../../components/Title";
 
 export function CreateTicket() {
   return (
     <div>
       <Title>Novo chamado</Title>
+
+      <form>
+        <FormLayout className="p-5">
+          <h3 className="text-base font-bold text-gray-200">Informações</h3>
+          <p className="text-gray-300 text-xs mt-1">
+            Configure os dias e horários em que você está disponível para atender chamados
+          </p>
+
+          <div className="mt-5 flex flex-col gap-5">
+            <div>
+              <Input
+                label="Título"
+                placeholder="Digite um título para o chamado"
+              />
+            </div>
+
+            <div>
+              <TextArea
+                id="description"
+                label="Descrição"
+                placeholder="Descreva o que está acontecendo"
+              />
+            </div>
+
+            <div>
+              <SelectInput id="service" label="Serviço">
+                <option value="support">Suporte Técnico</option>
+                <option value="support">Suporte Técnico</option>
+                <option value="support">Suporte Técnico</option>
+                <option value="support">Suporte Técnico</option>
+                <option value="support">Suporte Técnico</option>
+                <option value="support">Suporte Técnico</option>
+                <option value="support">Suporte Técnico</option>
+                <option value="support">Suporte Técnico</option>
+              </SelectInput>
+            </div>
+          </div>
+        </FormLayout>
+      </form>
     </div>
   );
 }
